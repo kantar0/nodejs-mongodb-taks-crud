0052f15b5592a74a8cd40786c1f5b16bea44c2c0
Kantar0 <thekantar0gmail.com>
2022-01-29 01:54:16 -0600

    Saving my project

---
+++ src/routes/index.routes.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import {
+  renderTasks,
+  createTask,
+  renderTaskEdit,
+  editTask,
+  deleteTask,
+  taskToggleDone,
+} from "../controllers/tasks.controllers";
+const router = Router();
+
+//index
+router.get("/", renderTasks);
+
+router.post("/tasks/add", createTask);
+
+router.get("/tasks/:id/toggleDone", taskToggleDone);
+
+router.get("/tasks/:id/edit", renderTaskEdit);
+
+router.post("/tasks/:id/edit", editTask);
+
+router.get("/tasks/:id/delete", deleteTask);
+
+
+export default router;
